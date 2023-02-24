98ce1693f68878cd7292312d679b324c3fac29b8
Brian T Gil <briantgil716@gmail.com>
2023-02-24 00:40:38 -0500

    extended sql

--- SQLiteConApp/Program.cs
+++ SQLiteConApp/Program.cs
@@ -12,10 +12,12 @@ namespace HelloWorldSample
             {
                 connection.Open();
 
+                //create table and insert initial data
                 var command = connection.CreateCommand();
                 command.CommandText =
                 @"
-                    CREATE TABLE user (
+                    DROP TABLE IF EXISTS USER;
+                    CREATE TABLE if not exists user (
                         id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                         name TEXT NOT NULL
                     );
@@ -24,58 +26,59 @@ namespace HelloWorldSample
                            (2, 'Alexander'),
                            (3, 'Nate');
                 ";
-                command.ExecuteNonQuery();
+                command.ExecuteNonQuery();  //C:\Users\...\Projects\SQLiteConApp\SQLiteConApp\bin\Debug\net6.0
 
+                //get a name from user to put into db
                 Console.Write("Name: ");
                 var name = Console.ReadLine();
-
-                #region snippet_Parameter
                 command.CommandText =
                 @"
                     INSERT INTO user (name)
                     VALUES ($name)
                 ";
-                command.Parameters.AddWithValue("$name", name);
-                #endregion
+                command.Parameters.AddWithValue("$name", name); //parameterized
                 command.ExecuteNonQuery();
 
+                //get and print last record
                 command.CommandText =
                 @"
                     SELECT last_insert_rowid()
                 ";
                 var newId = (long)command.ExecuteScalar();
-
                 Console.WriteLine($"Your new user ID is {newId}.");
             }
 
-            Console.Write("User ID: ");
-            var id = int.Parse(Console.ReadLine());
 
-            #region snippet_HelloWorld
             using (var connection = new SqliteConnection("Data Source=hello.db"))
             {
-                connection.Open();
+                //get query result
+                Console.Write("User ID: ");
+                var id1 = int.Parse(Console.ReadLine());
 
+                Console.Write("User ID: ");
+                var id2 = int.Parse(Console.ReadLine());
+
+                connection.Open();
                 var command = connection.CreateCommand();
                 command.CommandText =
                 @"
-                    SELECT name
+                    SELECT id, name
                     FROM user
-                    WHERE id = $id
+                    WHERE id in ($id1, $id2)
                 ";
-                command.Parameters.AddWithValue("$id", id);
+                command.Parameters.AddWithValue("$id1", id1);  //parameterized
+                command.Parameters.AddWithValue("$id2", id2);  //parameterized
 
+                //loop through query result
                 using (var reader = command.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        var name = reader.GetString(0);
-
+                        var name = reader.GetString(1);
                         Console.WriteLine($"Hello, {name}!");
                     }
                 }
             }
-            #endregion
 
             // Clean up
             //File.Delete("hello.db");
